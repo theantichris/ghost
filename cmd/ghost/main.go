3412a612f72a78b34d772507e3351c9b1361513f
Christopher Lamm <christopherlamm81@icloud.com>
2025-09-09 21:47:30 -0400

    create llmClient and app

--- cmd/ghost/main.go
+++ cmd/ghost/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/joho/godotenv"
+	"github.com/theantichris/ghost/internal/app"
+	"github.com/theantichris/ghost/internal/llm"
 )
 
 func main() {
@@ -10,8 +16,27 @@ func main() {
 	// Load CLI flags
 
 	// Create llm client
+	llmClient, err := llm.NewOllamaClient("http://localhost:11434", "llama2")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+
+		if errors.Is(err, llm.ErrURLEmpty) {
+			os.Exit(2)
+		}
+
+		if errors.Is(err, llm.ErrModelEmpty) {
+			os.Exit(3)
+		}
+
+		os.Exit(1)
+	}
 
 	// Create app
+	_, err = app.New(llmClient)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 
 	// Run app
 }
