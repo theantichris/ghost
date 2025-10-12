0b44921acc3b735484fe4abf122044e999ca387f
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-12 00:33:07 -0400

    implement basic command setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/theantichris/ghost
 
 go 1.24.2
+
+require github.com/urfave/cli/v3 v3.4.1
--- main.go
+++ main.go
@@ -2,10 +2,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
 )
 
 // main initializes and executes the Ghost CLI application using the fang framework.
 func main() {
-	fmt.Println("Hello, world!")
+	cmd := &cli.Command{
+		Name:  "ghost",
+		Usage: "send ghost a prompt",
+		Action: func(context.Context, *cli.Command) error {
+			fmt.Println("ghost system online")
+
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
