9e0e49f27e3f0e029f10337356e7b1357c1d419e
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-12 00:16:28 -0400

    scorch the earth

--- main.go
+++ main.go
@@ -2,16 +2,10 @@
 package main
 
 import (
-	"context"
-	"os"
-
-	"github.com/charmbracelet/fang"
-	"github.com/theantichris/ghost/cmd"
+	"fmt"
 )
 
 // main initializes and executes the Ghost CLI application using the fang framework.
 func main() {
-	if err := fang.Execute(context.Background(), cmd.Execute()); err != nil {
-		os.Exit(1)
-	}
+	fmt.Println("Hello, world!")
 }
