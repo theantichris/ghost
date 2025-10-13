451455c472f2809dfdacfb033236da0152b81ff9
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-12 22:53:34 -0400

    add TODOs for next steps

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+// TODO: setup config files
+// TODO: Ollama URL, default model, system prompt
+// TODO: handle LLM request and response
+
 // Run executes the root command (ghost) printing out a test string.
 func Run(ctx context.Context, args []string, output io.Writer, logger *log.Logger) error {
 	var prompt string
