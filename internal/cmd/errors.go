136115b62198a21854d5345fa6e98063c629c669
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-15 22:03:00 -0400

    change no prompt error text

--- internal/cmd/errors.go
+++ internal/cmd/errors.go
@@ -7,5 +7,5 @@ var (
 	ErrOutput = errors.New("failed to write output")
 
 	// ErrNoPrompt indicates a prompt wasn't given as a CLI argument.
-	ErrNoPrompt = errors.New("prompt not found")
+	ErrNoPrompt = errors.New("no prompt provided")
 )
