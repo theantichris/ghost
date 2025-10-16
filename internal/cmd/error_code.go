e5f4d741f84d6fc6b60697c5f059a5c87c1f30c4
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-16 19:25:11 -0400

    add comments for error codes

--- internal/cmd/error_code.go
+++ internal/cmd/error_code.go
@@ -4,20 +4,51 @@ package cmd
 type ExitCode int
 
 const (
-	Ok            ExitCode = 0
-	ExUsage       ExitCode = 64
-	ExDataErr     ExitCode = 65
-	ExNoInput     ExitCode = 66
-	ExNoUser      ExitCode = 67
-	ExNoHost      ExitCode = 68
+	// Ok/Success
+	Ok ExitCode = 0
+
+	// Bag flags/arguments, misuse of CLI
+	ExUsage ExitCode = 64
+
+	// Invalid input data/content
+	ExDataErr ExitCode = 65
+
+	// Missing input file/stream
+	ExNoInput ExitCode = 66
+
+	// User not found
+	ExNoUser ExitCode = 67
+
+	// Host not found
+	ExNoHost ExitCode = 68
+
+	// Service/resource unavailable
 	ExUnavailable ExitCode = 69
-	ExSoftware    ExitCode = 70
-	ExOsErr       ExitCode = 71
-	ExOsFile      ExitCode = 72
-	ExCantCreat   ExitCode = 73
-	ExIOErr       ExitCode = 74
-	ExTempFail    ExitCode = 75
-	ExProtocol    ExitCode = 76
-	ExNoPerm      ExitCode = 77
-	ExConfig      ExitCode = 78
+
+	// Internal error/bug
+	ExSoftware ExitCode = 70
+
+	// Operating system failure: fork, pipe, etc.
+	ExOsErr ExitCode = 71
+
+	// Operating system file missing/permission
+	ExOsFile ExitCode = 72
+
+	// Can't create output
+	ExCantCreat ExitCode = 73
+
+	// Input/output error
+	ExIOErr ExitCode = 74
+
+	// Temporary failure, retry may succeed
+	ExTempFail ExitCode = 75
+
+	// Protocol violation
+	ExProtocol ExitCode = 76
+
+	// Permission denied
+	ExNoPerm ExitCode = 77
+
+	// Bad/missing config
+	ExConfig ExitCode = 78
 )
