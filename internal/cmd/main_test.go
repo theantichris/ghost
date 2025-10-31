49cc9d31e0b8b10508f935a2ffc69010bafb4b13
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-30 21:04:48 -0400

    extend before() test

--- internal/cmd/main_test.go
+++ internal/cmd/main_test.go
@@ -6,34 +6,59 @@ import (
 	"testing"
 
 	"github.com/charmbracelet/log"
+	"github.com/theantichris/ghost/internal/llm"
 
 	"github.com/urfave/cli/v3"
 )
 
 func TestBefore(t *testing.T) {
-	logger := log.New(io.Discard)
-
-	cmd := cli.Command{}
-	cmd.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:  "host",
-			Value: "http://test.dev",
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "initializes LLM Client and adds to metadata",
 		},
-		&cli.StringFlag{
-			Name:  "model",
-			Value: "default:model",
-		},
-		&cli.StringFlag{
-			Name:  "vision-model",
-			Value: "vision:model",
-		},
-	}
-	cmd.Metadata = map[string]any{
-		"logger": logger,
 	}
 
-	_, err := before(context.Background(), &cmd)
-	if err != nil {
-		t.Errorf("expected no error, got %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			logger := log.New(io.Discard)
+
+			cmd := cli.Command{
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "host",
+						Value: "http://test.dev",
+					},
+					&cli.StringFlag{
+						Name:  "model",
+						Value: "default:model",
+					},
+					&cli.StringFlag{
+						Name:  "vision-model",
+						Value: "vision:model",
+					},
+				},
+				Metadata: map[string]any{
+					"logger": logger,
+				},
+			}
+
+			_, err := before(context.Background(), &cmd)
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+
+			actual := cmd.Metadata["llmClient"]
+
+			if actual == nil {
+				t.Fatalf("expected LLMClient, got nil")
+			}
+
+			if _, ok := actual.(llm.LLMClient); !ok {
+				t.Errorf("expected LLM Client to be of type LLMClient, got %v", actual)
+			}
+		})
 	}
+
 }
