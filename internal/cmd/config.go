be369d1c3b8b04a24a76c5e557819ce69e8e5f51
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-30 20:31:34 -0400

    change loadConfigFile to return StringSourcer to avoid nil pointer if no config file

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -9,23 +9,21 @@ import (
 	altsrc "github.com/urfave/cli-altsrc/v3"
 )
 
-// loadConfigFile attempts to load config.toml from ~/.config/ghost and returns a StringPtrSourcer.
-// If the config file does not exist, it returns an empty sourcer without error allowing the application to use default flag values. Returns ErrConfigFile if the home directory cannot be determined.
-func loadConfigFile(logger *log.Logger) (altsrc.StringPtrSourcer, error) {
+// loadConfigFile attempts to load config.toml from ~/.config/ghost and returns a StringSourcer.
+// If the config file does not exist, it returns an empty string without error allowing the application to use default flag values. Returns ErrConfigFile if the home directory cannot be determined.
+func loadConfigFile(logger *log.Logger) (altsrc.StringSourcer, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return altsrc.StringPtrSourcer{}, fmt.Errorf("%w", ErrConfigFile)
+		return "", fmt.Errorf("%w", ErrConfigFile)
 	}
 
 	configFile := filepath.Join(homeDir, ".config/ghost", "config.toml")
 
-	var sourcer altsrc.StringPtrSourcer
 	if _, err := os.Stat(configFile); err != nil {
-		logger.Debug("config file not found", "file", configFile)
+		logger.Debug("no config file to load", "file", configFile)
 	} else {
-		sourcer = altsrc.NewStringPtrSourcer(&configFile)
 		logger.Debug("loading config file", "file", configFile)
 	}
 
-	return sourcer, nil
+	return altsrc.StringSourcer(configFile), nil
 }
