7b5e7967d5c2f5c7a06ed9374f7d9b9cd678c24f
Christopher Lamm <christopherlamm81@gmail.com>
2025-12-27 11:17:19 -0500

    feat: theme error handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"image/color"
 	"io"
 	"os"
 
@@ -25,16 +26,21 @@ func main() {
 
 func theme(ld lipgloss.LightDarkFunc) fang.ColorScheme {
 	theme := fang.ColorScheme{
-		Title:       lipgloss.Color("#FF00FF"),
-		Description: lipgloss.Color("#00FFFF"),
-		Flag:        lipgloss.Color("#00FF00"),
-		Command:     lipgloss.Color("#FF0080"),
-		Argument:    lipgloss.Color("#80FF00"),
+		Title:        lipgloss.Color("#FF00FF"),
+		Description:  lipgloss.Color("#00FFFF"),
+		Flag:         lipgloss.Color("#00FF00"),
+		Command:      lipgloss.Color("#FF0080"),
+		Argument:     lipgloss.Color("#80FF00"),
+		ErrorHeader:  [2]color.Color{lipgloss.Color("#FF0000"), lipgloss.Color("#000000")},
+		ErrorDetails: lipgloss.Color("#FF6B6B"),
 	}
 
 	return theme
 }
 
 func errorHandler(w io.Writer, styles fang.Styles, err error) {
-	fmt.Fprintf(os.Stderr, "󱙜 ERROR: %s\n", err)
+	errorHeader := styles.ErrorHeader.Render("󱙜")
+	errorDetails := styles.ErrorText.Render(err.Error())
+
+	fmt.Fprintf(w, "%s\n%s\n", errorHeader, errorDetails)
 }
