1324611cad84b5391d531d80033047849bff31a5
Christopher Lamm <christopherlamm81@gmail.com>
2025-12-29 19:08:25 -0500

    test: refactor to table tests

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -7,29 +7,48 @@ import (
 )
 
 func TestInitMessages(t *testing.T) {
-	system := "system prompt"
-	prompt := "user prompt"
-
-	actual, err := initMessages(system, prompt, "")
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
+	tests := []struct {
+		name    string
+		system  string
+		prompt  string
+		format  string
+		wantErr bool
+		err     bool
+	}{
+		{
+			name:   "returns message history with no format",
+			system: "system prompt",
+			prompt: "user prompt",
+		},
 	}
 
-	expected := llm.ChatMessage{
-		Role:    "system",
-		Content: system,
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := initMessages(tt.system, tt.prompt, tt.format)
 
-	if actual[0] != expected {
-		t.Errorf("expected messages %v, got %v", expected, actual[0])
-	}
+			if !tt.wantErr {
+				if err != nil {
+					t.Fatalf("expected no error, got %v", err)
+				}
 
-	expected = llm.ChatMessage{
-		Role:    "user",
-		Content: prompt,
-	}
+				expected := llm.ChatMessage{
+					Role:    "system",
+					Content: tt.system,
+				}
 
-	if actual[1] != expected {
-		t.Errorf("expected messages %v, got %v", expected, actual[1])
+				if actual[0] != expected {
+					t.Errorf("expected message %v, got %v", expected, actual[0])
+				}
+
+				expected = llm.ChatMessage{
+					Role:    "user",
+					Content: tt.prompt,
+				}
+
+				if actual[1] != expected {
+					t.Errorf("expected messages %v, got %v", expected, actual[1])
+				}
+			}
+		})
 	}
 }
