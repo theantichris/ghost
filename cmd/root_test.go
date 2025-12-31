f145d671e6cf72b6894af6af96d843079b4e2aa8
Christopher Lamm <christopherlamm81@gmail.com>
2025-12-30 20:34:25 -0500

    test: add tests for validateFormat

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -53,6 +54,52 @@ func TestInitMessages(t *testing.T) {
 			actual := initMessages(tt.system, tt.prompt, tt.format)
 			if diff := cmp.Diff(tt.expected, actual); diff != "" {
 				t.Errorf("expected messages (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestValidateFormat(t *testing.T) {
+	tests := []struct {
+		name    string
+		format  string
+		wantErr bool
+		err     error
+	}{
+		{
+			name:   "does not return error for json",
+			format: "json",
+		},
+		{
+			name:   "does not return error for markdown",
+			format: "markdown",
+		},
+		{
+			name:    "returns error for invalid format",
+			format:  "butts",
+			wantErr: true,
+			err:     ErrInvalidFormat,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validateFormat(tt.format)
+
+			if !tt.wantErr {
+				if err != nil {
+					t.Fatalf("expected no error, got %v", err)
+				}
+			}
+
+			if tt.wantErr {
+				if err == nil {
+					t.Fatalf("expected error, got nil")
+				}
+
+				if !errors.Is(err, ErrInvalidFormat) {
+					t.Errorf("expected error %v, got %v", ErrInvalidFormat, err)
+				}
 			}
 		})
 	}
