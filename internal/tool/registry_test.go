6684c1a0a67ccfa7cef4b272ceddefc307216513
Christopher Lamm <christopherlamm81@gmail.com>
2026-01-18 15:01:12 -0500

    test: add tests for Execute()

--- internal/tool/registry_test.go
+++ internal/tool/registry_test.go
@@ -3,6 +3,7 @@ package tool
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"testing"
 
 	"github.com/theantichris/ghost/internal/llm"
@@ -63,5 +64,58 @@ func TestDefinitions(t *testing.T) {
 
 	if got[0].Function.Name != tool.Definition().Function.Name {
 		t.Fatalf("Definitions() name = %s, want %s", got[0].Function.Name, tool.Definition().Function.Name)
+	}
+}
+
+func TestExecute(t *testing.T) {
+	tests := []struct {
+		name       string
+		tool       mockTool
+		calledTool string
+		wantErr    bool
+		err        error
+	}{
+		{
+			name:       "executes tool",
+			tool:       mockTool{name: "mock tool", result: "mock result"},
+			calledTool: "mock tool",
+		},
+		{
+			name:       "returns error for unknown tool",
+			tool:       mockTool{name: "mock tool", result: "mock result"},
+			calledTool: "unknown tool",
+			wantErr:    true,
+			err:        ErrToolNotRegistered,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			registry := NewRegistry()
+
+			registry.Tools[tt.tool.Definition().Function.Name] = tt.tool
+
+			got, err := registry.Execute(context.Background(), tt.calledTool, json.RawMessage{})
+
+			if tt.wantErr {
+				if err == nil {
+					t.Error("Execute() err = nil, want error")
+				}
+
+				if !errors.Is(err, tt.err) {
+					t.Errorf("Execute() err = %v, want %v", err, tt.err)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("Execute() err = %v, want nil", err)
+			}
+
+			if got != tt.tool.result {
+				t.Errorf("Execute() got = %s, want %s", got, tt.tool.result)
+			}
+		})
 	}
 }
