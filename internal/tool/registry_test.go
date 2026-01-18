55a4513fa9e60e391c210a0bb7327655b8e5cc91
Christopher Lamm <christopherlamm81@gmail.com>
2026-01-18 14:39:29 -0500

    test: add test for Definitions()

--- internal/tool/registry_test.go
+++ internal/tool/registry_test.go
@@ -44,3 +44,24 @@ func TestRegister(t *testing.T) {
 		t.Errorf("Register() failed to register mock tool")
 	}
 }
+
+func TestDefinitions(t *testing.T) {
+	registry := NewRegistry()
+
+	tool := mockTool{
+		name:   "mock tool",
+		result: "mock result",
+	}
+
+	registry.Tools[tool.Definition().Function.Name] = tool
+
+	got := registry.Definitions()
+
+	if len(got) == 0 {
+		t.Fatal("Definitions() count = 0, want 1")
+	}
+
+	if got[0].Function.Name != tool.Definition().Function.Name {
+		t.Fatalf("Definitions() name = %s, want %s", got[0].Function.Name, tool.Definition().Function.Name)
+	}
+}
