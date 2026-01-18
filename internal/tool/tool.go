5ded5dffebafe07e46f0df2c06f376484f7bdc56
Christopher Lamm <christopherlamm81@gmail.com>
2026-01-18 15:44:46 -0500

    fix: typo

--- internal/tool/tool.go
+++ internal/tool/tool.go
@@ -7,7 +7,7 @@ import (
 	"github.com/theantichris/ghost/internal/llm"
 )
 
-// Tool is the interface that all the tools the LLM uses must implements.
+// Tool is the interface that all the tools the LLM uses must implement.
 type Tool interface {
 	Definition() llm.Tool
 	Execute(ctx context.Context, args json.RawMessage) (string, error)
