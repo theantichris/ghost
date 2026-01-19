dc6bdfd2306690a259d24fd599274e7b42b2d280
Christopher Lamm <christopherlamm81@gmail.com>
2026-01-18 23:23:23 -0500

    feat: stub out chat command, refactoring because pre-commit

---
+++ cmd/chat.go
@@ -0,0 +1,27 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+const (
+	chatUseText     = "chat"
+	chatShortText   = "starts ghost in chat mode"
+	chatLongText    = "starts ghost in chat mode, use :q to quit"
+	chatExampleText = "ghost chat"
+)
+
+func NewChatCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     chatUseText,
+		Short:   chatShortText,
+		Long:    chatLongText,
+		Example: chatExampleText,
+		Args:    cobra.NoArgs,
+		RunE:    runChat,
+	}
+
+	return cmd
+}
+
+func runChat(cmd *cobra.Command, args []string) error {
+	return nil
+}
