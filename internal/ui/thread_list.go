a0e91bb03967eee92ad228b3695407a6f827d250
Christopher Lamm <christopherlamm81@gmail.com>
2026-02-26 20:32:37 -0500

    refactor: move thread list item into thread list

--- internal/ui/thread_list.go
+++ internal/ui/thread_list.go
@@ -1,12 +1,33 @@
 package ui
 
 import (
+	"time"
+
 	"charm.land/bubbles/v2/list"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
 	"github.com/charmbracelet/log"
 	"github.com/theantichris/ghost/v3/internal/storage"
 )
+
+type threadItem struct {
+	thread storage.Thread
+}
+
+// Title returns the thread's title.
+func (item threadItem) Title() string {
+	return item.thread.Title
+}
+
+// Description returns the thread's formatted update timestamp.
+func (item threadItem) Description() string {
+	return item.thread.UpdatedAt.Format(time.ANSIC)
+}
+
+// FilterValue returns the title for the filter to search against.
+func (item threadItem) FilterValue() string {
+	return item.thread.Title
+}
 
 // ThreadListModel holds the state for the thread list.
 type ThreadListModel struct {
