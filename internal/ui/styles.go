64f080526bff450c10d5feea852c30845a30c224
Christopher Lamm <christopherlamm81@gmail.com>
2026-03-01 13:21:44 -0500

    feat: add accent color to borders

--- internal/ui/styles.go
+++ internal/ui/styles.go
@@ -12,23 +12,24 @@ const horizontalChrome = 2
 // verticalChrome is the size of vertical borders, margin, and padding.
 const verticalChrome = 6
 
-var baseBackground lipgloss.Style = lipgloss.NewStyle().Background(style.Bg3)
-
-var viewportStyle lipgloss.Style = lipgloss.NewStyle().
-	Background(style.Bg3).
-	Border(lipgloss.NormalBorder(), true)
-
 var textAreaStyles textarea.Styles = textarea.Styles{
 	Focused: textarea.StyleState{Base: baseBackground},
 	Blurred: textarea.StyleState{Base: baseBackground},
 }
 
+var baseBackground lipgloss.Style = lipgloss.NewStyle().Background(style.Bg3)
+
+var viewportStyle lipgloss.Style = lipgloss.NewStyle().
+	Background(style.Bg3).
+	Border(lipgloss.NormalBorder(), true).
+	BorderForeground(style.Accent0)
+
 var inputStyle lipgloss.Style = lipgloss.NewStyle().
 	Background(style.Bg3).
-	Border(lipgloss.NormalBorder(), true)
+	Border(lipgloss.NormalBorder(), true).
+	BorderForeground(style.Accent0)
 
 var statusBarStyle lipgloss.Style = lipgloss.NewStyle().
 	Background(style.Bg3).
-	Border(lipgloss.NormalBorder(), true)
-
-	// height - input height - status bar height = viewport height
+	Border(lipgloss.NormalBorder(), true).
+	BorderForeground(style.Accent0)
